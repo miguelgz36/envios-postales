b4eca85c0d343772ab03dd00caff8762abb0de08
Andres Diaz <andiazhe@unal.edu.co>
2020-12-10 14:14:15 -0500

    Agregar descuento de 5para paquetes

--- src/main/java/ar/com/grupoesfera/capacitacion/enviospostales/Calculadora.java
+++ src/main/java/ar/com/grupoesfera/capacitacion/enviospostales/Calculadora.java
@@ -6,11 +6,14 @@ public class Calculadora {
         if(numeroPaquetes<5){
             return 50;
         }
-        if(numeroPaquetes>=5 && numeroPaquetes<=10){
+        if(numeroPaquetes>=5 && numeroPaquetes<10){
             return  80;
         }
-        if(numeroPaquetes>10){
+        if(numeroPaquetes=>10 && numeroPaquetes<15){
             return  80 +(15)*(numeroPaquetes-10);
+        }
+        if(numeroPaquetes=>15){
+            return  (80 +(15)*(numeroPaquetes-10))*0.95 ;
         }
         return 0;
     }
