4a7f82363825e00b0d3526073a7ce442071f6f75
gnrodriguez <miguel.rodriguez@mercadolibre.com.co>
2020-12-10 13:55:27 -0500

    asignar transporte sin test

--- src/main/java/ar/com/grupoesfera/capacitacion/enviospostales/Asignador.java
+++ src/main/java/ar/com/grupoesfera/capacitacion/enviospostales/Asignador.java
@@ -1,4 +1,19 @@
 package ar.com.grupoesfera.capacitacion.enviospostales;
 
 public class Asignador {
+
+    public void asignarTransporte(Envio envio) throws Exception {
+        Double pesoTotal = envio.getPaquetes().stream()
+                .map(paquete -> paquete.getPeso())
+                .reduce((double) 0,(a, b) -> a + b );
+        if(pesoTotal < 5){
+            envio.setTransporte(Transporte.BICICLETA);
+        }else if(pesoTotal < 50){
+            envio.setTransporte(Transporte.AUTO);
+        }else if(pesoTotal < 150){
+            envio.setTransporte(Transporte.CAMIONETA);
+        }else{
+            throw new Exception("Debe ser envios con peso total menor a 150");
+        }
+    }
 }
